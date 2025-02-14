81891aca69321de7e0f6e35b29e8944eb1de82e6
Mohit Chand <chandmohit002@gmail.com>
2025-02-14 22:13:18 +0545

    login logout register

--- store/models.py
+++ store/models.py
@@ -8,6 +8,11 @@ class Category(models.Model):
     def __str__(self):
         return self.name
     
+        
+    # it makes our mobel name categories instead of categorys
+    class Meta:
+        verbose_name_plural = 'categories'
+    
 # Customer
 class Customer(models.Model):
     first_name = models.CharField(max_length=50) 
@@ -24,8 +29,14 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
     Category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
-    description = models.CharField(max_length=250, default='',blank=True, null=True)
+    description = models.CharField(max_length=5000, default='',blank=True, null=True)
     image = models.ImageField(upload_to='uploads/product/')
+    # Add Sales Stuff
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+
+    def __str__(self):
+        return self.name    
 
 
 # Customer Orders
--- store/views.py
+++ store/views.py
@@ -1,5 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from django import forms
+
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    products = Product.objects.all()
+    return render(request, 'home.html', {'products':products})
+
+def about(request):
+    return render(request, 'about.html', {})
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password) # authenticating the user
+        if user is not None: # Checking if authentication was successful
+            login(request, user)
+            messages.success(request, "You have been logged in successfully!")
+            return redirect('home')
+        else:
+            messages.success(request, "Wrong credentials...Try Again!")
+            return redirect('login')
+
+    else:
+        return render(request, 'login.html')
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged out...Thanks for shopping with us!")
+    return redirect('home')
+
+
+def register_user(request):
+    form = SignUpForm
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['usernaem']
+            password = form.cleaned_data['password1']
+            # login user
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You have been Registered successfully! Welcome...")
+            return redirect('home')
+        else:
+            messages.success(request, "Whoops! There was an error while registering Try Again...")
+            return redirect('register')      
+        
+    else:    
+        return render(request, 'register.html', {'form':form})
