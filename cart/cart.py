953ac11f8733bb8ebea23f9d746f667f0fe4c2e4
Mohit Chand <chandmohit002@gmail.com>
2025-03-12 14:55:12 +0545

    update cart

--- cart/cart.py
+++ cart/cart.py
@@ -1,3 +1,4 @@
+from store.models import Product
 class Cart():
     def __init__(self, request):
         self.session = request.session
@@ -11,4 +12,48 @@ class Cart():
 
         
         # Make sure cart is available on all pages of site
-        self.cart = cart 
+        self.cart = cart
+
+    def add(self, product, quantity):
+        product_id = str(product.id)
+        product_qty = str(quantity)
+
+        # Logic
+        if product_id in self.cart:
+            pass
+        else:
+            # self.cart[product_id] = {'price': str(product.price)}
+            self.cart[product_id] = int(product_qty)
+
+
+        self.session.modified = True
+
+    def __len__(self):
+        return len(self.cart)
+    
+    def get_prods(self):
+        # Get Ids from cart
+        product_ids = self.cart.keys()
+        # Use ids to lookup products in database model
+        products = Product.objects.filter(id__in=product_ids)
+        # return those looked up products
+        return products
+    
+    def get_quants(self):
+        quantities = self.cart
+        return quantities
+    
+    def update(self, product, quantity):
+        product_id = str(product)
+        product_qty = int(quantity)
+
+        # Get Cart
+        our_cart = self.cart
+
+        # update our cart(which is dictionary)
+        our_cart[product_id] = product_qty
+
+        self.session.modified = True
+
+        updated_cart = self.cart
+        return updated_cart
